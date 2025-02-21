c11917f0a0dcfe7ee28f11c9e5a80716e6aa3a09
Vitor Girotto <vjpgvjpgvjpgvjpg@gmail.com>
2025-02-20 21:28:05 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Mensagem enviada com sucesso!');
-    // Aqui você pode adicionar lógica para enviar o formulário ao servidor (usando AJAX, por exemplo)
-    this.reset();
-});
